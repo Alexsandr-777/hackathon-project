765e607c245a9390ee6d565b0085c9328e80e07e
Alexsandr-777 <sokollex777@gmail.com>
2025-06-29 14:47:25 +0300

    feat: добавлен death screen модуль, data, звуки и app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,6 @@
-// import './styles.css'
-// import { DeathScreenModule } from './modules/daeathScreenModule/deathScreenModule.js'
-// menu.add(new DeathScreenModule())
+
 import './styles.css'
-import { CustomMenu }        from './menu.js'
-import { DeathScreenModule }  from './modules/daeathScreenModule/deathScreenModule.js'
-import { ClicksModule }       from './modules/clicks.module.js'
-import { ShapeModule }        from './modules/shape.module.js'
-
+import { CustomMenu } from './menu.js'
+import { DeathScreenModule } from './modules/daeathScreenModule/deathScreenModule.js'
 const menu = new CustomMenu('#menu')
-
 menu.add(new DeathScreenModule())
-
-
